6c8e80a42c01b7bbcab74403cc10f857606d676f
Remigijus Jodelis <remi@samesystem.com>
2015-08-04 23:59:23 +0300

    all of Bind exercises

--- src/Course/Bind.hs
+++ src/Course/Bind.hs
@@ -23,12 +23,9 @@ import qualified Prelude as P
 --
 -- * The law of associativity
 --   `∀f g x. g =<< (f =<< x) ≅ ((g =<<) . f) =<< x`
+
 class Apply f => Bind f where
-  -- Pronounced, bind.
-  (=<<) ::
-    (a -> f b)
-    -> f a
-    -> f b
+  (=<<) :: (a -> f b) -> f a -> f b -- Pronounced, bind.
 
 infixr 1 =<<
 
@@ -63,63 +60,56 @@ infixr 1 =<<
 --
 -- >>> ((*) <*> (+2)) 3
 -- 15
-(<*>) ::
-  Bind f =>
-  f (a -> b)
-  -> f a
-  -> f b
-(<*>) =
-  error "todo: Course.Bind#(<*>)"
+
+(<*>) :: Bind f => f (a -> b) -> f a -> f b
+(<*>) f a = (<$> a) =<< f
+-- (<*>) f a = (=<<) (\g -> (<$>) (\b -> g b) a) f
+-- (\f' -> (\a' -> f' a') <$> a) =<< f
 
 infixl 4 <*>
+
 
 -- | Binds a function on the Id monad.
 --
 -- >>> (\x -> Id(x+1)) =<< Id 2
 -- Id 3
+
 instance Bind Id where
-  (=<<) ::
-    (a -> Id b)
-    -> Id a
-    -> Id b
-  (=<<) =
-    error "todo: Course.Bind (=<<)#instance Id"
+  (=<<) :: (a -> Id b) -> Id a -> Id b
+  (=<<) = bindId
+
 
 -- | Binds a function on a List.
 --
 -- >>> (\n -> n :. n :. Nil) =<< (1 :. 2 :. 3 :. Nil)
 -- [1,1,2,2,3,3]
+
 instance Bind List where
-  (=<<) ::
-    (a -> List b)
-    -> List a
-    -> List b
-  (=<<) =
-    error "todo: Course.Bind (=<<)#instance List"
+  (=<<) :: (a -> List b) -> List a -> List b
+  (=<<) = flatMap
+
 
 -- | Binds a function on an Optional.
 --
 -- >>> (\n -> Full (n + n)) =<< Full 7
 -- Full 14
+
 instance Bind Optional where
-  (=<<) ::
-    (a -> Optional b)
-    -> Optional a
-    -> Optional b
-  (=<<) =
-    error "todo: Course.Bind (=<<)#instance Optional"
+  (=<<) :: (a -> Optional b) -> Optional a -> Optional b
+  (=<<) = bindOptional
+
 
 -- | Binds a function on the reader ((->) t).
 --
 -- >>> ((*) =<< (+10)) 7
 -- 119
+
 instance Bind ((->) t) where
-  (=<<) ::
-    (a -> ((->) t b))
-    -> ((->) t a)
-    -> ((->) t b)
-  (=<<) =
-    error "todo: Course.Bind (=<<)#instance ((->) t)"
+  (=<<) :: (a -> ((->) t b)) -> ((->) t a) -> ((->) t b)
+  (=<<) f a = \t -> f (a t) t
+
+-- (a -> (t -> b)) -> (t -> a) -> (t -> b)
+
 
 -- | Flattens a combined structure to a single structure.
 --
@@ -134,12 +124,10 @@ instance Bind ((->) t) where
 --
 -- >>> join (+) 7
 -- 14
-join ::
-  Bind f =>
-  f (f a)
-  -> f a
-join =
-  error "todo: Course.Bind#join"
+
+join :: Bind f => f (f a) -> f a
+join = (=<<) id
+
 
 -- | Implement a flipped version of @(=<<)@, however, use only
 -- @join@ and @(<$>)@.
@@ -147,29 +135,22 @@ join =
 --
 -- >>> ((+10) >>= (*)) 7
 -- 119
-(>>=) ::
-  Bind f =>
-  f a
-  -> (a -> f b)
-  -> f b
-(>>=) =
-  flip (=<<)
+
+(>>=) :: Bind f => f a -> (a -> f b) -> f b
+(>>=) a f = join (f <$> a)
 
 infixl 1 >>=
+
 
 -- | Implement composition within the @Bind@ environment.
 -- Pronounced, kleisli composition.
 --
 -- >>> ((\n -> n :. n :. Nil) <=< (\n -> n+1 :. n+2 :. Nil)) 1
 -- [2,2,3,3]
-(<=<) ::
-  Bind f =>
-  (b -> f c)
-  -> (a -> f b)
-  -> a
-  -> f c
-(<=<) =
-  error "todo: Course.Bind#(<=<)"
+
+(<=<) :: Bind f => (b -> f c) -> (a -> f b) -> (a -> f c)
+(<=<) f g = \a -> f =<< g a
+
 
 infixr 1 <=<
 
